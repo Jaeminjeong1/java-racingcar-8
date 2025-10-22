16faa854e165b78c2f67615cfcfdb94c2e1c917b
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2025-10-22 20:09:09 +0900

    feat: RacingCar 도메인(이름 검증) 구현

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -1,4 +1,31 @@
 package racingcar.domain;
 
 public class RacingCar {
+
+    private String carName;
+    private Long distance;
+
+    public RacingCar(String carName) {
+        this.carName = carName;
+        this.distance = 0L;
+    }
+
+    public RacingCar from (String carName) {
+        validateCarName(carName);
+        return new RacingCar(carName);
+    }
+
+    private void validateCarName(String carName) {
+        if (carName.isBlank() || carName.length() > 5) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public Long getDistance() {
+        return distance;
+    }
 }
