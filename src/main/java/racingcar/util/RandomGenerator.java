a3ad26b851872dda61144e6453a011d9958bf6bb
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2025-10-22 20:13:40 +0900

    feat: RandomGenerator 랜덤값 받는 기능 구현

--- src/main/java/racingcar/util/RandomGenerator.java
+++ src/main/java/racingcar/util/RandomGenerator.java
@@ -1,9 +1,15 @@
 package racingcar.util;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RandomGenerator {
 
     private RandomGenerator() {}
     public static RandomGenerator create() {
         return new RandomGenerator();
+    }
+
+    public int generateRandomNum() {
+        return Randoms.pickNumberInRange(0, 9);
     }
 }
