dee1e8f20478383abea50b2f614f9041725a102f
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2025-10-22 20:25:16 +0900

    feat: 레이스 시작 출력기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,8 +2,15 @@ package racingcar.view;
 
 public class OutputView {
 
+    private static final String RACE_START = "실행 결과";
+
     private OutputView() {}
     public static OutputView create() {
         return new OutputView();
+    }
+
+    public void printRaceStart() {
+        System.out.println();
+        System.out.println(RACE_START);
     }
 }
