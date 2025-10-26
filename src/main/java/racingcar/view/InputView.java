93ca4d4212936829d19692b207f26aa403804524
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2025-10-26 20:22:28 +0900

    refactor: 라운드 숫자 형변환 과졍에서 오토박싱 문제 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -31,7 +31,7 @@ public class InputView {
         validateEmpty(userInput);
         validateNumberPattern(userInput);
 
-        Long roundNum = Long.parseLong(userInput); //valuOf, 오토박싱
+        Long roundNum = Long.valueOf(userInput);
         validatePositive(roundNum);
 
         return roundNum;
