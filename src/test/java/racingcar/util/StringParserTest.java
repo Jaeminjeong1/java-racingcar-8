f27b1ac8feb9d8bbf8d419e4c5d51e44a5849ace
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2025-10-27 11:45:36 +0900

    test: StringParser 테스트 코드 작성

---
+++ src/test/java/racingcar/util/StringParserTest.java
@@ -0,0 +1,41 @@
+package racingcar.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class StringParserTest {
+
+    @Test
+    void 구분자_기능 () {
+        String input = "pobi,woni,jun";
+        String[] result = StringParser.parse(input);
+
+        assertThat(result).containsExactly("pobi", "woni", "jun");
+    }
+
+    @Test
+    void 공백_포함_구분자_기능() {
+        String input = " pobi , woni , jun ";
+        String[] result = StringParser.parse(input);
+
+        assertThat(result).containsExactly(" pobi ", " woni ", " jun ");
+    }
+
+    @Test
+    void 구분자_앞이_빈_경우() {
+        String input = ",pobi,woni";
+        String[] result = StringParser.parse(input);
+
+        assertThat(result).containsExactly("", "pobi", "woni");
+    }
+
+    @Test
+    void 구분자_뒤가_빈_경우() {
+        String input = "pobi,woni,";
+        String[] result = StringParser.parse(input);
+
+        assertThat(result).containsExactly("pobi", "woni");
+    }
+}
