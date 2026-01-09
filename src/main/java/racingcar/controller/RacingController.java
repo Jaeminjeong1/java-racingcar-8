f921a60a76496ef062e0f7064f8a0cb1e8196758
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2026-01-09 12:22:56 +0900

    feat: Application 구조 구현

---
+++ src/main/java/racingcar/controller/RacingController.java
@@ -0,0 +1,23 @@
+package racingcar.controller;
+
+import racingcar.service.RacingService;
+
+public class RacingController {
+
+    private final RacingService racingService;
+
+    public RacingController(RacingService racingService) {
+        this.racingService = racingService;
+    }
+
+    public void start() {
+        // 경주할 자동차 입력받기
+        // 자동차 생성
+
+        // 시도할 횟수 입력받기
+        // 입력받은 횟수만큼 경기 실행
+        // 한 경기마다 결과 출력
+
+        // 최종 우승자 출력
+    }
+}
